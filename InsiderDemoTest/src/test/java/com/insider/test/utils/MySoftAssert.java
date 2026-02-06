056681a57bfcaffc4a656b88a7fcd5467d7bb087
Ahmet <yasir.aksoy@outlook.com>
2026-02-06 06:50:14 +0300

    MySoftAssert class to take a screenshot when test fails

---
+++ InsiderDemoTest/src/test/java/com/insider/test/utils/MySoftAssert.java
@@ -0,0 +1,13 @@
+package com.insider.test.utils;
+import org.testng.asserts.IAssert;
+import org.testng.asserts.SoftAssert;
+
+public class MySoftAssert extends SoftAssert {
+
+    @Override
+    public void onAssertFailure(IAssert<?> assertCommand, AssertionError ex) {
+        String methodName = assertCommand.getMessage();
+
+        CommonMethods.takeScreenshot("SoftAssert_Failure_" + methodName+".png");
+    }
+}
