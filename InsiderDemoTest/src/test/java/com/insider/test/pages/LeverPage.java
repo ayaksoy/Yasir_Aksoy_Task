b7ef7ae95869f984125ec11fce81c8d57a13fa14
Ahmet <yasir.aksoy@outlook.com>
2026-02-06 09:40:57 +0300

    leverPage

---
+++ InsiderDemoTest/src/test/java/com/insider/test/pages/LeverPage.java
@@ -0,0 +1,18 @@
+package com.insider.test.pages;
+import com.insider.test.utils.CommonMethods;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LeverPage extends CommonMethods {
+    @FindBy(xpath = "//a[text()='Apply for this job']")
+    public WebElement applyButton;
+
+    public LeverPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public boolean isApplyButtonDisplayed() {
+        return applyButton.isDisplayed();
+    }
+}
