bef1c4f46b70d995440fcc067d1b469d64729601
Ahmet <yasir.aksoy@outlook.com>
2026-02-06 06:50:37 +0300

    Home test

---
+++ InsiderDemoTest/src/test/java/com/insider/test/pages/HomePage.java
@@ -0,0 +1,35 @@
+package com.insider.test.pages;
+
+import com.insider.test.testbase.BaseClass;
+import com.insider.test.utils.CommonMethods;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends CommonMethods {
+
+    @FindBy(css = ".header-logo  svg")
+    public WebElement headerLogo;
+
+    @FindBy(id = "navigation")
+    public WebElement navigationBar;
+
+    @FindBy(xpath = "//h1[contains(text(),'Be unstoppable')]")
+    public WebElement heroSection;
+
+    @FindBy(xpath = "//h2[contains(text(),'TRUSTED BY 2,000+ CUSTOMERS')]")
+    public WebElement trustedBySection;
+
+    @FindBy(className = "homepage-core-differentiators")
+    public WebElement coreDifferentiators;
+
+    @FindBy(className = "homepage-capabilities")
+    public WebElement capabilitiesSection;
+
+    @FindBy(tagName = "footer")
+    public WebElement footer;
+
+    public HomePage() {
+        PageFactory.initElements(BaseClass.driver, this);
+    }
+}
---
+++ InsiderDemoTest/src/test/java/com/insider/test/testcases/HomeTest.java
@@ -0,0 +1,45 @@
+package com.insider.test.testcases;
+
+import com.insider.test.utils.CommonMethods;
+import com.insider.test.utils.ConfigsReader;
+import com.insider.test.utils.MySoftAssert;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomeTest extends CommonMethods {
+    @Test
+    public void verifyHomePageBlocks() {
+        driver.get(ConfigsReader.getProperty("url"));
+        Assert.assertTrue(driver.getTitle().contains("Insider"), "Page title wrong!");
+
+
+        MySoftAssert softAssert = new MySoftAssert();
+        scrollToElement(homePage.navigationBar);
+        waitForVisibility(homePage.navigationBar);
+        softAssert.assertTrue(homePage.navigationBar.isDisplayed(), "Navbar not loaded!");
+
+        scrollToElement(homePage.heroSection);
+        waitForVisibility(homePage.heroSection);
+        softAssert.assertTrue(homePage.heroSection.isDisplayed(), "Hero section not loaded!");
+
+        scrollToElement(homePage.trustedBySection);
+        waitForVisibility(homePage.trustedBySection);
+        softAssert.assertTrue(homePage.trustedBySection.isDisplayed(), "TrustedBy section not loaded!");
+
+        scrollToElement(homePage.coreDifferentiators);
+        waitForVisibility(homePage.coreDifferentiators);
+        softAssert.assertTrue(homePage.coreDifferentiators.isDisplayed(), "coreDifferentiators section not loaded!");
+
+        scrollToElement(homePage.capabilitiesSection);
+        waitForVisibility(homePage.capabilitiesSection);
+        softAssert.assertTrue(homePage.capabilitiesSection.isDisplayed(), "capabilities section not loaded!");
+
+        scrollToElement(homePage.footer);
+        waitForVisibility(homePage.footer);
+        softAssert.assertTrue(homePage.footer.isDisplayed(), "Footer Section not loaded!");
+
+
+
+        softAssert.assertAll();
+    }
+}
